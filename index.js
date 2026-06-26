1edabada1704ddc2f440fd220fa69aa90cab89f5
dantemartinez-6792 <dante_martinez6792@hotmail.com>
2026-06-26 09:17:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const mysql = require('mysql2/promise'); // versión con promesas
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware para recibir JSON y manejar CORS
 app.use(cors());
